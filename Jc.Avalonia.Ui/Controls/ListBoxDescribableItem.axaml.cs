759bd0020a63c7a29ca0268257c1b38605cdc92f
Joseph Sawyer <joseph@jcsawyer.com>
2025-07-31 18:40:25 +0100

    feat: add button and listbox styles

---
+++ Jc.Avalonia.Ui/Controls/ListBoxDescribableItem.axaml.cs
@@ -0,0 +1,26 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jc.Avalonia.Ui.Controls;
+
+public partial class ListBoxDescribableItem : ListBoxItem
+{
+    public static readonly StyledProperty<string> ItemProperty = AvaloniaProperty.Register<ListBoxDescribableItem, string>(
+        nameof(Item));
+
+    public string Item
+    {
+        get => GetValue(ItemProperty);
+        set => SetValue(ItemProperty, value);
+    }
+
+    public static readonly StyledProperty<string> DescriptionProperty = AvaloniaProperty.Register<ListBoxDescribableItem, string>(
+        nameof(Description));
+
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+}
--- Jc.Avalonia.Ui/Properties/AssemblyInfo.cs
+++ Jc.Avalonia.Ui/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Jc.Avalonia.Ui")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Jc.Avalonia.Ui.Controls")]
